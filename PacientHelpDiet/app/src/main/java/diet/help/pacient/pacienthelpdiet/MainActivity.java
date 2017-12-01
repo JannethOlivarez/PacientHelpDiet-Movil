c9642220ae35b523ad4ebe965e35425f863ec2e0
Mauricio <henrymantillals@gmail.com>
2017-12-01 03:10:36 -0500

    Inicio de seccion
    Creacion de la pantalla para el inicio de session

--- PacientHelpDiet/app/src/main/java/diet/help/pacient/pacienthelpdiet/MainActivity.java
+++ PacientHelpDiet/app/src/main/java/diet/help/pacient/pacienthelpdiet/MainActivity.java
@@ -1,13 +1,78 @@
 package diet.help.pacient.pacienthelpdiet;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText txt_Email,txt_Password;
+    FirebaseAuth.AuthStateListener mAuthListener;
+    Fise
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt_Email=(EditText) findViewById(R.id.txt_email);
+        txt_Password=(EditText) findViewById(R.id.txt_password);
+        mAuthListener= new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user!=null){
+                    Toast.makeText(getApplicationContext(),"Correcto"+user.getEmail(),Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(),"Cerrar"+user.getEmail(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+    }
+
+    public void Login(View view){
+        login(txt_Email.getText().toString(),txt_Password.getText().toString());
+    }
+    public void Usuarios(View view){
+        registrar(txt_Email.getText().toString(),txt_Password.getText().toString());
+    }
+
+    private void login(String email,String password){
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password);
+    }
+
+    private void registrar(String email,String password){
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Creado",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener!=null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
     }
 }
